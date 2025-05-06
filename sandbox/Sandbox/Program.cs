ab165caee3cd53647e304be747082c5783fd70a3
Thomas Garrett <thomasgarrett455@gmail.com>
2025-05-06 08:55:55 -0600

    commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
+     static double AddNumbers(double a, double b)
+      {
+        return a + b;
+      }
+
+      static void DisplayGreeting()
+      {
+        Console.WriteLine("Hello Bob, how are you?");
+      }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Sandbox World!");
+     double total = AddNumbers(123.456, 654.321);
+     Console.WriteLine(total);
+     DisplayGreeting();
     }
 }
