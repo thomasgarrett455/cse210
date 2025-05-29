89e5f3245c71fa467d258011876631d42d5080eb
Thomas Garrett <thomasgarrett455@gmail.com>
2025-05-29 09:47:38 -0600

    Word Counter

---
+++ WordCounter/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+
+    public static void Main(string[] args)
+    {
+        WordCounter wordCounter = new WordCounter("This is a text sentence to allow text testing my new class");
+        wordCounter.DisplayWords();
+        int count = wordCounter.CountSingleWord("text");
+        Console.WriteLine(count);
+    }
+}
