ba501aaacd30130685c834a72738063a6a6c2504
Thomas Garrett <thomasgarrett455@gmail.com>
2025-07-02 08:01:08 -0600

    commit

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,5 @@
 // To exceed requirements I added a level feature to the program. It adds up the users points 
-// to determine what level they are. The levels get hader and harder to pass each level up.
+// to determine what level they are. The levels get harder and harder to pass each level up.
 class Program
 {
     static void Main(string[] args)
