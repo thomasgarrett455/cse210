a18d7dd20c4c54d8c98c58a90f91c0b37dd4b865
Thomas Garrett <thomasgarrett455@gmail.com>
2025-07-09 09:26:51 -0600

    Foubndation 1

---
+++ final/Foundation1/Comments.cs
@@ -0,0 +1,21 @@
+class Comments
+{
+    private string _name;
+    private string _text;
+
+    public Comments(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetText()
+    {
+        return _text;
+    }
+}
