e560de40edbf333f33d275f9d145615909a5107c
Thomas Garrett <thomasgarrett455@gmail.com>
2025-07-10 20:43:23 -0600

    foundation 4 done

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -38,6 +38,4 @@ public abstract class Activity
 
     public abstract double GetPace();
 
-
-
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,15 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Running myRunning = new Running("03 Nov 2025", 60, 5);
-        Console.WriteLine(myRunning.ActivityInfo());
-        Console.WriteLine();
 
         Bike myBike = new Bike("03 Nov 2025", 60, 10);
-        Console.WriteLine(myBike.ActivityInfo());
-        Console.WriteLine();
 
         Swim mySwim = new Swim("03 Nov 2025", 30, 20);
-        Console.WriteLine(mySwim.ActivityInfo());
-        
+
+        List<Activity> myEvents = new List<Activity>();
+        myEvents.Add(myRunning);
+        myEvents.Add(myBike);
+        myEvents.Add(mySwim);
+
+        foreach (Activity activity in myEvents)
+        {
+            DisplayActivityInformation(activity);
+            Console.WriteLine();
+        }
+    }
+
+    public static void DisplayActivityInformation(Activity activity)
+    {
+        Console.WriteLine(activity.ActivityInfo());
     }
 }
